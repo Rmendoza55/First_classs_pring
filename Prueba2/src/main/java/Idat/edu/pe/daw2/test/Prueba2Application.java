8a384f0d5b5fb92a67eac3bdf227f429994ce987
Rmendoza55 <107291096+Rmendoza55@users.noreply.github.com>
2022-10-11 21:35:32 -0500

    Primer comit en el repositorio

---
+++ Prueba2/src/main/java/Idat/edu/pe/daw2/test/Prueba2Application.java
@@ -0,0 +1,13 @@
+package Idat.edu.pe.daw2.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba2Application.class, args);
+	}
+
+}
---
+++ Prueba2/src/test/java/Idat/edu/pe/daw2/test/Prueba2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Idat.edu.pe.daw2.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
